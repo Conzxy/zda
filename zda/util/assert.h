4f27b61e90381644d38fee149a7415dad82d9cc6
Conzxy <1967933844@qq.com>
2023-06-18 17:47:58 +0800

    chore: assert macro

---
+++ zda/util/assert.h
@@ -0,0 +1,10 @@
+// SPDX-LICENSE-IDENTIFIER: MIT
+#ifndef _ZDA_ASSERT_H_
+#define _ZDA_ASSERT_H_
+
+#include <assert.h>
+
+#define zda_assert0(cond)     assert((cond) && (msg))
+#define zda_assert(cond, msg) assert((cond) && (msg));
+
+#endif /* Header guard */
