4c2927dd047d9d46fedfd9a16ac443711391485d
Conzxy <1967933844@qq.com>
2023-06-23 14:51:25 +0800

    feat(list): No sentinel APIs

--- test/list_test.cc
+++ test/list_test.cc
@@ -7,33 +7,103 @@ typedef struct int_node {
   zda_list_node_t node;
 } int_node_t;
 
-#define prepare_list(n)                                                                            \
-  zda_list_header_t header;                                                                        \
-  zda_list_node_header_init(&header);                                                              \
-  int_node_t nodes[n];                                                                             \
-  for (int i = 0; i < n; ++i) {                                                                    \
-    zda_list_push_front(&header, &nodes[i].node);                                                  \
-    nodes[i].val = i;                                                                              \
-  }
+int_node_t *new_int_node() { return (int_node_t *)malloc(sizeof(int_node_t)); }
 
-TEST(list_test, insert)
+static void prepare_list(zda_list_header_t *list, int n)
 {
-  constexpr int n = 10;
-  prepare_list(n)
-
-  zda_list_iterate(&header)
-  {
-    int_node_t *int_node = zda_list_get_entry(int_node_t);
-    printf("val = %d\n", int_node->val);
+  for (int i = 0; i < n; ++i) {
+    auto node = new_int_node();
+    zda_list_push_back(list, &node->node);
+    node->val = i;
   }
 }
 
+TEST(list_test, insert)
+{
+  constexpr int     n = 10;
+  zda_list_header_t header;
+  zda_list_header_init(&header);
+  prepare_list(&header, n);
+  zda_list_iterate(&header)
+  {
+    int_node_t *int_node = zda_list_entry(pos, int_node_t);
+    printf("val = %d\n", int_node->val);
+  }
+
+  zda_list_destroy(&header, int_node_t, free);
+}
+
+static zda_inline zda_bool int_equal(int x, int y) { return x == y; }
+
 TEST(list_test, search)
 {
-  prepare_list(100)
+  zda_list_header_t header;
+  zda_list_header_init(&header);
+  prepare_list(&header, 100);
 
-  int_node_t *p_entry = NULL;
-  zda_list_search(&header, int_node_t, val, 40, p_entry);
+  int_node_t *p_entry;
+  zda_list_search(&header, 40, int_node_t, val, int_equal, p_entry);
 
+  ASSERT_TRUE(p_entry);
   EXPECT_TRUE(p_entry->val == 40);
+  zda_list_destroy(&header, int_node_t, free);
+}
+
+TEST(list_test, remove)
+{
+  zda_list_header_t header;
+  zda_list_header_init(&header);
+  prepare_list(&header, 100);
+
+  int i = 0;
+  while (!zda_list_is_empty(&header)) {
+    auto front = zda_list_front(&header);
+    auto ent   = zda_list_entry(front, int_node_t);
+    EXPECT_EQ(ent->val, i);
+    ++i;
+    zda_list_pop_front(&header);
+    free(ent);
+  }
+}
+
+static void prepare_list2(zda_list_header2_t *header, int n)
+{
+  for (int i = 0; i < n; ++i) {
+    auto node = new_int_node();
+    zda_list_push_back2(header, &node->node);
+    node->val = i;
+  }
+}
+
+TEST(list_test2, insert)
+{
+  zda_list_header2_t header;
+  zda_list_header2_init(&header);
+  prepare_list2(&header, 100);
+  zda_list_iter2 iter = zda_list_first(&header);
+
+  int i = 0;
+  for (; !zda_list_is_terminator2(&header, &iter); zda_list_iter_inc2(&iter)) {
+    auto entry = zda_list_entry(iter.node, int_node_t);
+    printf("val = %d\n", entry->val);
+    EXPECT_EQ(entry->val, i);
+    ++i;
+  }
+
+  zda_list_destroy2(&header, int_node_t, free);
+}
+
+TEST(list_test2, search)
+{
+  zda_list_header2_t header;
+  zda_list_header2_init(&header);
+  prepare_list2(&header, 100);
+
+  for (int i = 0; i < 100; ++i) {
+    int_node_t *p_entry;
+    zda_list_search2(&header, i, int_node_t, val, int_equal, p_entry);
+    ASSERT_TRUE(p_entry);
+    EXPECT_EQ(p_entry->val, i);
+  }
+  zda_list_destroy2(&header, int_node_t, free);
 }
--- zda/list.h
+++ zda/list.h
@@ -2,23 +2,39 @@
 #ifndef _ZDA_LIST_H__
 #define _ZDA_LIST_H__
 
+#include "zda/util/assert.h"
 #include "zda/util/macro.h"
 #include "zda/util/container_of.h"
+#include "zda/util/bool.h"
 
 #ifdef __cplusplus
 EXTERN_C_BEGIN
 #endif
 
+/**
+ * @brief The node that stores the user data
+ */
 typedef struct zda_list_node {
   struct zda_list_node *next;
   struct zda_list_node *prev;
 } zda_list_node_t;
 
-typedef struct zda_list_node zda_list_header;
-typedef zda_list_node_t      zda_list_header_t;
+/**
+ * @brief Double-linked-list with a sentinel header that don't store data
+ */
+typedef struct zda_list_header {
+  zda_list_node node;
+} zda_list_header_t;
 
-#define zda_list_node_entry(p_node, type) container_of(p_node, type, node)
-#define zda_list_get_entry(type)          zda_list_node_entry(pos, type)
+/**
+ * @brief Double-linked-list with a heaeder that stores user data
+ */
+typedef struct zda_list_header2 {
+  zda_list_node *node;
+} zda_list_header2_t;
+
+#define zda_list_node_entry(p_node, type, node) container_of(p_node, type, node)
+#define zda_list_entry(pos, type)               zda_list_node_entry(pos, type, node)
 
 /* `static inline` is necessary for ISO C89/C90.
  * In C89, The compiler will think the non-static inline function is a usual function */
@@ -28,20 +44,38 @@ static zda_inline void zda_list_node_sentinel_init(zda_list_node_t *sentinel)
   sentinel->next = sentinel;
 }
 
-#define zda_list_node_header_init zda_list_node_sentinel_init
+static zda_inline void zda_list_header_init(zda_list_header_t *header)
+{
+  zda_list_node_sentinel_init(&header->node);
+}
+
+static zda_inline zda_bool zda_list_is_empty(zda_list_header_t *header)
+{
+  return header->node.prev == &header->node;
+}
+
+static zda_inline zda_list_node_t *zda_list_front(zda_list_header_t *header)
+{
+  return header->node.next;
+}
+
+static zda_inline zda_list_node_t *zda_list_back(zda_list_header_t *header)
+{
+  return header->node.prev;
+}
 
 /******************************************/
 /* Insert APIs                            */
 /******************************************/
 
-static zda_inline void zda_list_node_insert_before(zda_list_node_t *pos, zda_list_node_t *new_node)
+static zda_inline void zda_list_insert_before(zda_list_node_t *pos, zda_list_node_t *new_node)
 {
   new_node->next  = pos;
   new_node->prev  = pos->prev;
   pos->prev->next = new_node;
   pos->prev       = new_node;
 }
-static zda_inline void zda_list_node_insert_after(zda_list_node_t *pos, zda_list_node_t *new_node)
+static zda_inline void zda_list_insert_after(zda_list_node_t *pos, zda_list_node_t *new_node)
 {
   new_node->next  = pos->next;
   new_node->prev  = pos;
@@ -49,47 +83,51 @@ static zda_inline void zda_list_node_insert_after(zda_list_node_t *pos, zda_list
   pos->next       = new_node;
 }
 
-#define zda_list_insert_after zda_list_node_insert_after
-
 static zda_inline void zda_list_push_front(zda_list_header_t *header, zda_list_node_t *new_node)
 {
-  zda_list_node_insert_after(header, new_node);
+  zda_list_insert_after(&header->node, new_node);
 }
 
 static zda_inline void zda_list_push_back(zda_list_header_t *header, zda_list_node_t *new_node)
 {
-  zda_list_node_insert_before(header, new_node);
+  zda_list_insert_before(&header->node, new_node);
 }
 
 /*******************************************/
 /* Remove APIs                             */
 /*******************************************/
 
-static zda_inline void zda_list_node_remove(zda_list_node_t *old_node)
+static zda_inline void zda_list_remove(zda_list_node_t *old_node)
 {
   old_node->next->prev = old_node->prev;
   old_node->prev->next = old_node->next;
 }
 
-#define zda_list_remove zda_list_node_remove
-
 #define zda_list_destroy(header, type, free_cb)                                                    \
   do {                                                                                             \
-    zda_list_iterate(header)                                                                       \
-    {                                                                                              \
-      free_cb(container_of(pos, type, node));                                                      \
+    zda_list_header_t *__header = header;                                                          \
+    zda_list_node_t   *next;                                                                       \
+    for (zda_list_node_t *pos = __header->node.next; pos != &__header->node;) {                    \
+      next = pos->next;                                                                            \
+      free_cb(zda_list_entry(pos, type));                                                          \
+      pos = next;                                                                                  \
     }                                                                                              \
-    (header)->prev = (header)->next = ZDA_NULL;                                                    \
   } while (0)
 
-static zda_inline void zda_list_pop_front(zda_list_header_t *header, zda_list_node_t *old_node)
+#define zda_list_destroy_init(header, type, free_cb)                                               \
+  do {                                                                                             \
+    zda_list_destroy(header, type, free_cb);                                                       \
+    zda_list_header_init(header);                                                                  \
+  } while (0)
+
+static zda_inline void zda_list_pop_front(zda_list_header_t *header)
 {
-  zda_list_node_remove(header->next);
+  zda_list_remove(header->node.next);
 }
 
-static zda_inline void zda_list_pop_back(zda_list_node_t *header, zda_list_node_t *old_node)
+static zda_inline void zda_list_pop_back(zda_list_header_t *header)
 {
-  zda_list_node_remove(header->prev);
+  zda_list_remove(header->node.prev);
 }
 
 /***********************************/
@@ -97,22 +135,172 @@ static zda_inline void zda_list_pop_back(zda_list_node_t *header, zda_list_node_
 /***********************************/
 
 #define zda_list_iterate(header)                                                                   \
-  for (zda_list_node_t *pos = (header)->next; pos != (header); pos = pos->next)
+  for (zda_list_node_t *pos = (header)->node.next; pos != &((header)->node); pos = pos->next)
 
-#define zda_list_search(header, type, member, val, p_entry)                                        \
+#define zda_list_search(header, val, type, member, cmp, p_entry)                                   \
   do {                                                                                             \
-    zda_list_iterate(header)                                                                       \
+    zda_list_header_t *_header = header;                                                           \
+    zda_list_iterate(_header)                                                                      \
+    p_entry = NULL;                                                                                \
+    zda_list_iterate(_header)                                                                      \
     {                                                                                              \
-      type *entry = zda_list_node_entry(pos, type);                                                \
-      if (entry->member == val) {                                                                  \
+      type *entry = zda_list_entry(pos, type);                                                     \
+      if (cmp(entry->member, val)) {                                                               \
         p_entry = entry;                                                                           \
         break;                                                                                     \
       }                                                                                            \
     }                                                                                              \
   } while (0)
 
+/**************************************/
+/* No sentinel version */
+/**************************************/
+static zda_inline void zda_list_header2_init(zda_list_header2_t *header) { header->node = NULL; }
+
+static zda_inline zda_bool zda_list_is_empty2(zda_list_header2_t *header)
+{
+  return header->node == NULL;
+}
+
+static zda_inline void zda_list_push_front2(zda_list_header2_t *header, zda_list_node_t *node)
+{
+  if (header->node) {
+    zda_list_insert_after(header->node, node);
+  } else {
+    header->node = node;
+    node->prev = node->next = node;
+  }
+}
+
+static zda_inline void zda_list_push_back2(zda_list_header2_t *header, zda_list_node_t *node)
+{
+  if (header->node) {
+    zda_list_insert_before(header->node, node);
+  } else {
+    header->node = node;
+    node->prev = node->next = node;
+  }
+}
+
+typedef struct zda_list_iter2 {
+  zda_list_node_t *node;
+  int              pass;
+} zda_list_iter2_t;
+
+static zda_inline zda_list_iter2 zda_list_first(zda_list_header2_t *header)
+{
+  zda_list_iter2 iter;
+  iter.node = header->node;
+  iter.pass = 0;
+  return iter;
+}
+
+static zda_inline zda_list_iter2 zda_list_terminator(zda_list_header2_t *header)
+{
+  zda_list_iter2 iter;
+  iter.node = header->node;
+  iter.pass = 1;
+  return iter;
+}
+
+static zda_inline void zda_list_iter_inc2(zda_list_iter2 *iter) { iter->node = iter->node->next; }
+
+static zda_inline zda_bool zda_list_is_terminator2(zda_list_header2_t *header, zda_list_iter2 *iter)
+{
+  /* if (iter->node == NULL) return zda_true;
+  if (iter->node == header->node) {
+    if (iter->pass == 0) {
+      iter->pass = 1;
+      return zda_false;
+    }
+    return zda_true;
+  }
+  return zda_false; */
+  auto ret = (!iter->node) || (iter->node == header->node && iter->pass > 0);
+  if (!ret) iter->pass = 1;
+  return ret;
+}
+
+static zda_inline zda_list_node_t *zda_list_front2(zda_list_header2_t *header)
+{
+  return header->node;
+}
+
+static zda_inline zda_list_node_t *zda_list_back2(zda_list_header2_t *header)
+{
+  return header->node->prev;
+}
+
+static zda_inline void zda_list_pop_front2(zda_list_header2_t *header)
+{
+  assert(header->node);
+  if (header->node->next) {
+    zda_list_remove(header->node);
+  } else {
+    header->node = NULL;
+  }
+}
+
+static zda_inline void zda_list_pop_back2(zda_list_header2_t *header)
+{
+  assert(header->node);
+  if (header->node->prev) {
+    zda_list_remove(header->node->prev);
+  } else {
+    header->node = NULL;
+  }
+}
+
+static zda_inline void zda_list_remove2(zda_list_header2_t *header, zda_list_node_t *node)
+{
+  assert(node);
+  if (node->prev == node) {
+    header->node = NULL;
+  } else {
+    zda_list_remove(node);
+  }
+}
+
+#define zda_list_search2(header, val, type, member, cmp, p_entry)                                  \
+  do {                                                                                             \
+    zda_list_header2_t *__header = header;                                                         \
+    zda_list_node_t    *first    = __header->node;                                                 \
+    p_entry                      = NULL;                                                           \
+    for (; first != NULL;) {                                                                       \
+      type *cur_entry = zda_list_entry(first, type);                                               \
+      if (cmp(cur_entry->member, val)) {                                                           \
+        p_entry = cur_entry;                                                                       \
+        break;                                                                                     \
+      }                                                                                            \
+      first = first->next;                                                                         \
+      if (first == __header->node) {                                                               \
+        break;                                                                                     \
+      }                                                                                            \
+    }                                                                                              \
+  } while (0)
+
+#define zda_list_destroy2(header, type, free_cb)                                                   \
+  do {                                                                                             \
+    zda_list_header2_t *__header = header;                                                         \
+    zda_list_node_t    *first    = __header->node;                                                 \
+    for (; first != NULL;) {                                                                       \
+      zda_list_node_t *next = first->next;                                                         \
+      free_cb(zda_list_entry(first, type));                                                        \
+      first = next;                                                                                \
+      if (first == __header->node) {                                                               \
+        break;                                                                                     \
+      }                                                                                            \
+    }                                                                                              \
+  } while (0)
+
+#define zda_list_destroy_init2(header, type, free_cb)                                              \
+  do {                                                                                             \
+    zda_list_destroy2(header, type, free_cb);                                                      \
+    zda_list_header2_init(header);                                                                 \
+  } while (0)
+
 #ifdef __cplusplus
 EXTERN_C_END
 #endif
 
-#endif
+#endif /* Header guard */
