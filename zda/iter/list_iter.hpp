4439bf3e0133c7c5c7d381e4ab81f0c6e913bc2a
Conzxy <1967933844@qq.com>
2023-07-09 16:55:27 +0800

    feat(list): 适配Const迭代器

--- zda/iter/list_iter.hpp
+++ zda/iter/list_iter.hpp
@@ -6,60 +6,122 @@
 namespace zda {
 
 template <typename EntryType>
-struct ListIterator {
+struct ListConstIterator {
  public:
-  ListIterator(zda_list_node_t *node) noexcept
-    : node_(node)
-  {
-  }
+    ListConstIterator(zda_list_node_t const *node) noexcept
+      : node_((zda_list_node_t *)node)
+    {
+    }
 
-  ListIterator &operator++() noexcept
-  {
-    node_ = node_->next;
-    return *this;
-  }
+    ListConstIterator &operator++() noexcept
+    {
+        node_ = node_->next;
+        return *this;
+    }
 
-  ListIterator operator++(int) noexcept
-  {
-    auto ret = *this;
-    node_    = node_->next;
-    return ret;
-  }
+    ListConstIterator operator++(int) noexcept
+    {
+        auto ret = *this;
+        node_    = node_->next;
+        return ret;
+    }
 
-  ListIterator &operator--() noexcept
-  {
-    node_ = node_->prev;
-    return *this;
-  }
+    ListConstIterator &operator--() noexcept
+    {
+        node_ = node_->prev;
+        return *this;
+    }
 
-  ListIterator operator--(int) noexcept
-  {
-    auto ret = *this;
-    node_    = node_->prev;
-    return ret;
-  }
+    ListConstIterator operator--(int) noexcept
+    {
+        auto ret = *this;
+        node_    = node_->prev;
+        return ret;
+    }
 
-  EntryType       &operator*() noexcept { return *zda_list_entry(node_, EntryType); }
-  EntryType const &operator*() const noexcept { return *(static_cast<ListIterator *>(this)); }
+    EntryType const &operator*() noexcept { return *zda_list_entry(node_, EntryType); }
+    EntryType const &operator*() const noexcept
+    {
+        return *(static_cast<ListConstIterator *>(this));
+    }
 
-  EntryType       *operator->() noexcept { return zda_list_entry(node_, EntryType); }
-  EntryType const *operator->() const noexcept { return zda_list_entry(node_, EntryType); }
+    EntryType const *operator->() noexcept { return zda_list_entry(node_, EntryType); }
+    EntryType const *operator->() const noexcept { return zda_list_entry(node_, EntryType); }
 
-  zda_list_node_t       *node() noexcept { return node_; }
-  zda_list_node_t const *node() const noexcept { return node_; }
+    zda_list_node_t       *node() noexcept { return node_; }
+    zda_list_node_t const *node() const noexcept { return node_; }
 
-  friend zda_inline bool operator==(ListIterator lhs, ListIterator rhs) noexcept
-  {
-    return lhs.node_ == rhs.node_;
-  }
+    friend zda_inline bool operator==(ListConstIterator lhs, ListConstIterator rhs) noexcept
+    {
+        return lhs.node_ == rhs.node_;
+    }
 
-  friend zda_inline bool operator!=(ListIterator lhs, ListIterator rhs) noexcept
-  {
-    return !(lhs == rhs);
-  }
+    friend zda_inline bool operator!=(ListConstIterator lhs, ListConstIterator rhs) noexcept
+    {
+        return !(lhs == rhs);
+    }
 
  private:
-  zda_list_node_t *node_;
+    zda_list_node_t *node_;
+};
+
+template <typename EntryType>
+struct ListIterator {
+ public:
+    ListIterator(zda_list_node_t *node) noexcept
+      : node_(node)
+    {
+    }
+
+    operator ListConstIterator<EntryType>() noexcept { return node_; }
+
+    ListIterator &operator++() noexcept
+    {
+        node_ = node_->next;
+        return *this;
+    }
+
+    ListIterator operator++(int) noexcept
+    {
+        auto ret = *this;
+        node_    = node_->next;
+        return ret;
+    }
+
+    ListIterator &operator--() noexcept
+    {
+        node_ = node_->prev;
+        return *this;
+    }
+
+    ListIterator operator--(int) noexcept
+    {
+        auto ret = *this;
+        node_    = node_->prev;
+        return ret;
+    }
+
+    EntryType       &operator*() noexcept { return *zda_list_entry(node_, EntryType); }
+    EntryType const &operator*() const noexcept { return *(static_cast<ListIterator *>(this)); }
+
+    EntryType       *operator->() noexcept { return zda_list_entry(node_, EntryType); }
+    EntryType const *operator->() const noexcept { return zda_list_entry(node_, EntryType); }
+
+    zda_list_node_t       *node() noexcept { return node_; }
+    zda_list_node_t const *node() const noexcept { return node_; }
+
+    friend zda_inline bool operator==(ListIterator lhs, ListIterator rhs) noexcept
+    {
+        return lhs.node_ == rhs.node_;
+    }
+
+    friend zda_inline bool operator!=(ListIterator lhs, ListIterator rhs) noexcept
+    {
+        return !(lhs == rhs);
+    }
+
+ private:
+    zda_list_node_t *node_;
 };
 
 } // namespace zda
--- zda/list.hpp
+++ zda/list.hpp
@@ -10,47 +10,50 @@ namespace zda {
 template <typename Entry, typename Free = LibcFree<Entry>>
 class List : protected Free {
  public:
-  using iterator   = ListIterator<Entry>;
-  using entry_type = Entry;
+    using iterator       = ListIterator<Entry>;
+    using const_iterator = ListConstIterator<Entry>;
+    using entry_type     = Entry;
 
-  List() noexcept { zda_list_header_init(&header_); }
-  ~List() noexcept { zda_list_destroy(&header_, Entry, (*((Free *)this))); }
+    List() noexcept { zda_list_header_init(&header_); }
+    ~List() noexcept { zda_list_destroy(&header_, Entry, (*((Free *)this))); }
 
-  bool is_empty() const noexcept { return zda_list_is_empty(&header_); }
+    bool is_empty() const noexcept { return zda_list_is_empty(&header_); }
 
-  Entry       &front() noexcept { return *zda_list_front(&header_); }
-  Entry const &front() const noexcept { return *zda_list_front((zda_list_header_t *)&header_); }
+    Entry       &front() noexcept { return *zda_list_front(&header_); }
+    Entry const &front() const noexcept { return *zda_list_front((zda_list_header_t *)&header_); }
 
-  Entry       &back() noexcept { return *zda_list_back(&header_); }
-  Entry const &back() const noexcept { return *zda_list_back((zda_list_header_t *)&header_); }
+    Entry       &back() noexcept { return *zda_list_back(&header_); }
+    Entry const &back() const noexcept { return *zda_list_back((zda_list_header_t *)&header_); }
 
-  void insert_before(iterator pos, zda_list_node_t *node) noexcept
-  {
-    zda_list_insert_before(pos.node(), node);
-  }
+    void insert_before(const_iterator pos, zda_list_node_t *node) noexcept
+    {
+        zda_list_insert_before(pos.node(), node);
+    }
 
-  void insert_after(iterator pos, zda_list_node_t *node) noexcept
-  {
-    zda_list_insert_after(pos.node(), node);
-  }
+    void insert_after(const_iterator pos, zda_list_node_t *node) noexcept
+    {
+        zda_list_insert_after(pos.node(), node);
+    }
 
-  void push_front(zda_list_node_t *node) noexcept { zda_list_push_front(&header_, node); }
+    void push_front(zda_list_node_t *node) noexcept { zda_list_push_front(&header_, node); }
 
-  void push_back(zda_list_node_t *node) noexcept { zda_list_push_back(&header_, node); }
+    void push_back(zda_list_node_t *node) noexcept { zda_list_push_back(&header_, node); }
 
-  void pop_front() noexcept { zda_list_pop_front(&header_); }
+    void pop_front() noexcept { zda_list_pop_front(&header_); }
 
-  void pop_back() noexcept { zda_list_pop_back(&header_); }
+    void pop_back() noexcept { zda_list_pop_back(&header_); }
 
-  void remove(iterator iter) noexcept { zda_list_remove(iter.node()); }
+    void remove(const_iterator iter) noexcept { zda_list_remove(iter.node()); }
 
-  iterator begin() const noexcept { return header_.node.next; }
-  iterator end() noexcept { return &header_.node; }
+    const_iterator begin() const noexcept { return header_.node.next; }
+    iterator       begin() noexcept { return header_.node.next; }
+    const_iterator end() const noexcept { return &header_.node; }
+    iterator       end() noexcept { return &header_.node; }
 
-  zda_list_header_t &rep() noexcept { return header_; }
+    zda_list_header_t &rep() noexcept { return header_; }
 
  private:
-  zda_list_header_t header_;
+    zda_list_header_t header_;
 };
 
 } // namespace zda
