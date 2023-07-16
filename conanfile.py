2417fdf0bca84c39f7ba0e926e560e4bbf6285c6
Conzxy <1967933844@qq.com>
2023-07-16 14:55:31 +0800

    chore: 使用自己生成的Config.cmake和Targets.cmake文件

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class ZdaRecipe(ConanFile):
     author = "Conzxy zengxiaoyu0@gmail.com"
     url = "https://github.com/Conzxy/zda"
     description = "Practical toolbox of data structures and algorithms"
-    topics = ("bidirectional-linked-list", "algorithms", "data-structures")
+    topics = ("algorithms", "data-structures")
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
@@ -36,15 +36,18 @@ class ZdaRecipe(ConanFile):
         self.folders.source = '.'
         self.folders.build = os.path.join("build", str(self.settings.build_type))
         self.folders.generators = os.path.join(self.folders.build, "generators")
-
-        self.cpp.source.includedirs = ['.']
-        self.cpp.build.libdirs = ['.']
-        self.cpp.build.libs = ["zda"]
+        self.cpp.build.builddirs.append(".")
+        # self.cpp.source.includedirs = ['.']
+        # self.cpp.build.libdirs = ['.']
+        # self.cpp.build.libs = ["zda"]
 
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
+        # conan自己好像设置了BUILD_SHARED_LIBS，但是没用
+        # 强制设置缓存变量可以起作用（和命令行一样）
+        tc.cache_variables["BUILD_SHARED_LIBS"] = self.options.shared
         tc.generate()
 
     def build(self):
@@ -57,4 +60,6 @@ class ZdaRecipe(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["zda"]
+        self.cpp_info.builddirs.append('lib/cmake/zda')
+        self.cpp_info.set_property("cmake_find_mode", "none")
+
